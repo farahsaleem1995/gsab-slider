00d900a8934d1223e498303daf29577687e2b42c
Farah <farahexa2020@gmail.com>
2021-05-31 11:56:17 +0300

    add prev and next animation

--- src/index.js
+++ src/index.js
@@ -1,94 +1,244 @@
 import "./sass/main.scss";
 import { gsap } from "gsap";
 
-const card1 = {
-  element: document.querySelector(".card-1"),
-};
-const card2 = {
-  element: document.querySelector(".card-2"),
-};
-const card3 = {
-  element: document.querySelector(".card-3"),
-};
-
-card1.height = card1.element.clientHeight;
-card1.width = card1.element.clientWidth;
-
-card2.height = card2.element.clientHeight;
-card2.width = card2.element.clientWidth;
-card2.x = {
-  new: getTranslateX(card1.element),
-  origin: getTranslateX(card2.element),
-};
-card2.y = {
-  new: -((card1.height - card2.height) / 2),
-  origin: 0,
-};
-
-card3.height = card3.element.clientHeight;
-card3.width = card3.element.clientWidth;
-card3.x = {
-  new: getTranslateX(card2.element),
-  origin: getTranslateX(card3.element),
-};
-card3.y = {
-  new: -((card2.height - card3.height) / 2),
-  origin: 0,
-};
-
-gsap
-  .timeline()
-  .to(".card-3", {
-    xPercent: "-20",
-    opacity: 0,
-    duration: 0.3,
-    delay: 2,
-  })
-  .set(".card-1", {
-    xPercent: "-20",
-    opacity: 0,
-  })
-  .set(".card-3", {
-    xPercent: 0,
-    opacity: 1,
-  })
-  .set(".card-2", {
-    x: `${card2.x.new}`,
-    y: `${card2.y.new}`,
-    height: `${card1.height}px`,
-    width: `${card1.width}px`,
-  })
-  .set(".card-3", {
-    x: `${card3.x.new}`,
-    y: `${card3.y.new}`,
-    height: `${card2.height}px`,
-    width: `${card2.width}px`,
-  })
-  .to(".card-2", {
-    x: `${card2.x.origin}`,
-    y: `${card2.y.origin}`,
-    height: `${card2.height}px`,
-    width: `${card2.width}px`,
-    duration: 0.3,
-  })
-  .to(".card-3", {
-    x: `${card3.x.origin}`,
-    y: `${card3.y.origin}`,
-    height: `${card3.height}px`,
-    width: `${card3.width}px`,
-    duration: 0.3,
-    delay: -0.3,
-  })
-  .to(".card-1", {
-    xPercent: 0,
-    opacity: 1,
-    duration: 0.3,
-    delay: -0.2,
-  });
-
-function getTranslateX(element) {
-  const style = getComputedStyle(element);
-  const matrix = new WebKitCSSMatrix(style.transform);
-
-  return matrix.m41;
+/**
+ * @property {Number} new
+ * @property {Number} origin
+ */
+class SliderPosition {
+  /**
+   *
+   * @param {Number} originPos
+   * @param {Number} newPos
+   */
+  constructor(originPos, newPos) {
+    this.origin = originPos;
+    this.new = newPos;
+  }
 }
+
+class SliderCard {
+  /**
+   *
+   * @param {HTMLElement} element
+   */
+  constructor(element) {
+    this.element = element;
+  }
+
+  /**
+   * Get element height
+   *
+   * @returns {Number}
+   */
+  height = () => {
+    return this.element.clientHeight;
+  };
+
+  /**
+   * Get element width
+   *
+   * @returns {Number}
+   */
+  width = () => {
+    return this.element.clientWidth;
+  };
+
+  /**
+   * Get element x axis transaltion according to a specifc element
+   *
+   * @param {SliderCard} accordingTo
+   * @returns {SliderPosition}
+   */
+  x = (accordingTo) => {
+    return {
+      new: this.getTranslateX(accordingTo.element),
+      origin: this.getTranslateX(this.element),
+    };
+  };
+
+  /**
+   * Get element y axis transaltion according to a specifc element
+   *
+   * @param {SliderCard} accordingTo
+   * @returns {SliderPosition}
+   */
+  y = (accordingTo) => {
+    return {
+      new: -((accordingTo.height() - this.height()) / 2),
+      origin: 0,
+    };
+  };
+
+  getTranslateX = (element) => {
+    const style = getComputedStyle(element);
+    const matrix = new WebKitCSSMatrix(style.transform);
+
+    return matrix.m41;
+  };
+}
+
+class Slider {
+  /**
+   *
+   * @param {SliderCard} primary
+   * @param {SliderCard} secondary
+   * @param {SliderCard} tertiary
+   */
+  constructor(primary, secondary, tertiary) {
+    this.primary = primary;
+    this.secondary = secondary;
+    this.tertiary = tertiary;
+
+    this.progress = false;
+  }
+
+  prevAnimate = () => {
+    return gsap
+      .timeline({
+        onStart: () => {
+          this.progress = true;
+        },
+        onComplete: () => {
+          this.progress = false;
+        },
+      })
+      .to(this.tertiary.element, {
+        xPercent: "-20",
+        opacity: 0,
+        duration: 0.3,
+      })
+      .set(this.primary.element, {
+        xPercent: "-20",
+        opacity: 0,
+      })
+      .set(this.tertiary.element, {
+        xPercent: 0,
+        opacity: 1,
+      })
+      .set(this.secondary.element, {
+        x: `${this.secondary.x(this.primary).new}`,
+        y: `${this.secondary.y(this.primary).new}`,
+        height: `${this.primary.height()}px`,
+        width: `${this.primary.width()}px`,
+      })
+      .set(this.tertiary.element, {
+        x: `${this.tertiary.x(this.secondary).new}`,
+        y: `${this.tertiary.y(this.secondary).new}`,
+        height: `${this.secondary.height()}px`,
+        width: `${this.secondary.width()}px`,
+      })
+      .to(this.secondary.element, {
+        y: `${this.secondary.y(this.primary).origin}`,
+        x: `${this.secondary.x(this.primary).origin}`,
+        height: `${this.secondary.height()}px`,
+        width: `${this.secondary.width()}px`,
+        duration: 0.3,
+      })
+      .to(this.tertiary.element, {
+        x: `${this.tertiary.x(this.secondary).origin}`,
+        y: `${this.tertiary.y(this.secondary).origin}`,
+        height: `${this.tertiary.height()}px`,
+        width: `${this.tertiary.width()}px`,
+        duration: 0.3,
+        delay: -0.3,
+      })
+      .to(this.primary.element, {
+        xPercent: 0,
+        opacity: 1,
+        duration: 0.3,
+        delay: -0.2,
+      });
+  };
+
+  nextAnimate = () => {
+    return gsap
+      .timeline({
+        onStart: () => {
+          this.progress = true;
+        },
+        onComplete: () => {
+          this.progress = false;
+        },
+      })
+      .to(this.primary.element, {
+        xPercent: 20,
+        opacity: 0,
+        duration: 0.3,
+      })
+      .set(this.tertiary.element, {
+        scale: 0.2,
+        xPercent: -50,
+        opacity: 0,
+      })
+      .set(this.primary.element, {
+        xPercent: 0,
+        opacity: 1,
+      })
+      .set(this.primary.element, {
+        x: `${this.primary.x(this.secondary).new}`,
+        y: `${this.primary.y(this.secondary).new}`,
+        height: `${this.secondary.height()}px`,
+        width: `${this.secondary.width()}px`,
+      })
+      .set(this.secondary.element, {
+        x: `${this.secondary.x(this.tertiary).new}`,
+        y: `${this.secondary.y(this.tertiary).new}`,
+        height: `${this.tertiary.height()}px`,
+        width: `${this.tertiary.width()}px`,
+      })
+      .to(this.primary.element, {
+        y: `${this.primary.y(this.primary).origin}`,
+        x: `${this.primary.x(this.primary).origin}`,
+        height: `${this.primary.height()}px`,
+        width: `${this.primary.width()}px`,
+        duration: 0.3,
+      })
+      .to(this.secondary.element, {
+        y: `${this.secondary.y(this.secondary).origin}`,
+        x: `${this.secondary.x(this.secondary).origin}`,
+        height: `${this.secondary.height()}px`,
+        width: `${this.secondary.width()}px`,
+        duration: 0.3,
+        delay: -0.3,
+      })
+      .to(this.tertiary.element, {
+        scale: 1,
+        xPercent: 0,
+        opacity: 1,
+        duration: 0.3,
+        delay: -0.3,
+      });
+  };
+}
+
+const prevBtn = document.querySelector(".ctrl .prev");
+const nextBtn = document.querySelector(".ctrl .next");
+
+const slider = new Slider(
+  new SliderCard(document.querySelector(".card-1")),
+  new SliderCard(document.querySelector(".card-2")),
+  new SliderCard(document.querySelector(".card-3"))
+);
+
+prevBtn.addEventListener("click", () => {
+  if (!slider.progress) {
+    slider.prevAnimate();
+  }
+});
+
+nextBtn.addEventListener("click", () => {
+  if (!slider.progress) {
+    slider.nextAnimate();
+  }
+});
+
+// setTimeout(() => {
+//   const slider = new Slider(
+//     new SliderCard(document.querySelector(".card-1")),
+//     new SliderCard(document.querySelector(".card-2")),
+//     new SliderCard(document.querySelector(".card-3"))
+//   );
+
+//   slider.nextAnimate();
+// }, 4000);
